58e8f3caa0be8ddbdf0568ead2eab8600d90db7b
upendra <bvru.dakuri@gmail.com>
2020-02-27 23:15:49 +0530

    Section 7 Assignment

---
+++ RandomInsultGenerator.js
@@ -0,0 +1,6 @@
+var randomBodyParts=['neck','nose','eyes','ears'];
+var randomAdjectives=['drinking','sleeping','eating'];
+var randomBodyPart=randomBodyParts[Math.floor(Math.random()*3)];
+console.log(randomBodyPart);
+var randomAdjective=randomAdjectives[Math.floor(Math.random()*2)];
+console.log(randomAdjective);
