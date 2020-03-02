625d86bb210bb7e3d59c880713b194e01a7531f6
upendra-dakuri <57784475+upendra-dakuri@users.noreply.github.com>
2020-03-02 12:21:10 +0530

    Delete CatYearsCalculation.js

--- CatYearsCalculation.js
+++
@@ -1,7 +0,0 @@
-var humanAge=20;
-var firstYears=2;
-var laterYears=humanAge-firstYears;
-firstYears=firstYears*25;
-laterYears=laterYears*4;
-var totalAge=firstYears+laterYears;
-console.log('My Age in Cat Years is:'+totalAge);
