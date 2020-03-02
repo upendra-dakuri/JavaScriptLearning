fbdc3f6e692b08c7d57d42af68d6aa28bd4b34d9
upendra-dakuri <57784475+upendra-dakuri@users.noreply.github.com>
2020-03-02 12:14:43 +0530

    Delete Arrays.js

--- Arrays.js
+++
@@ -1,8 +0,0 @@
-var fruits=['apple','mango','banana','grapes'];
-console.log(fruits.length);
-console.log(fruits.pop());
-console.log(fruits.push('orange'));
-console.log(fruits);
-console.log(fruits.shift());
-console.log(fruits.unshift('kiwi'));
-console.log(fruits);
