ee427e676bc535d010d00945971c301181f832ec
upendra-dakuri <57784475+upendra-dakuri@users.noreply.github.com>
2020-03-02 12:21:36 +0530

    Delete KelvinToFahrenheit.js

--- KelvinToFahrenheit.js
+++
@@ -1,8 +0,0 @@
-function convertKTF(kel)
-{
-    //var kelvinTemp=kel;
-    var celsiusTemp=kel-273.15;
-    var fahrenheitTemp=celsiusTemp*(9/5)+32;
-    return fahrenheitTemp
-}
-console.log("Temperature in Fahrenheit is :"+convertKTF(301));
