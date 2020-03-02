d1503c71479d222c838f5648129b85af82a8f6c7
upendra-dakuri <57784475+upendra-dakuri@users.noreply.github.com>
2020-03-02 12:20:42 +0530

    Delete ArraysMethods.js

--- ArraysMethods.js
+++
@@ -1,7 +0,0 @@
-var firstnames=['omni','wyse'];
-var lastnames=['intern','javascript'];
-var tech=['java','.net','php'];
-var fullnames=firstnames.concat(lastnames);
-var total=firstnames.concat(lastnames,tech);
-console.log(fullnames);
-console.log(total);
